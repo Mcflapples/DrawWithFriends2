a0eaf87bfe3228f16dc904a692c598a3b5a036fe
Jake Gilbert <jjake.gilbert@outlook.com>
2019-06-06 17:51:53 +1000

    back to the state where currently ggetting an Err_connection refused by socket.io

--- routes/api/drawing.js
+++ routes/api/drawing.js
@@ -75,63 +75,35 @@ class Sessions {
 
 
   //Start the Socket connection which will push and emit all ActiveUsers Positions to other users
-  //io.on("connection", socket => {
-  //  while(1){
-  //        console.log("A connection has been established");
-  //       const sessionKeys = Object.keys(sessions);
-  //       const cursorPositions = [];
-  //       //Loop and update cursorPositions for each User
-  //       for(let i =0, n = sessionKeys.length; i < n; i ++){
-  //          const key = sessionKeys[i];
-  //          const session = sessions[key];
-  //          cursorPositions.push({
-  //              x: session.getMouseX(),
-  //              y: session.getMouseY(),
-  //              userName: session.getName(),
-  //              key: session.getName()
-  //          });
-  //       }
-  //       //Emit - send the cursor positions to all other clients connected
-  //       socket.emit("cursor", cursorPositions);
-  //      }
-  //      });
-  //    io.on("line", data => {
-  //        console.log("A line is being drawn");
-  //        const session = sessions[data.sessionKey];
-  //        session.resetTimer();
-  //        session.setMouseX(data.x);
-  //        session.setMouseY(data.y);
-  //        const lineCoordinates = data.lineCoordinates;
-  //        io.emit("line", {
-  //            lineWidth: data.lineWidth,
-  //            lineColour: data.lineColour,
-  //            lineCoordinates
-  //        });
-  //    });
-
-  io.on('connection', socket => {
+  io.on("connection", socket => {
     setInterval(() => {
-      const sessionKeys = Object.keys(sessions);
-      const cursorPositions = [];
-      for(let i = 0, n = sessionKeys.length; i < n; i ++) {
-        const key = sessionKeys[i];
-        const session = sessions[key];
-        cursorPositions.push({
-          x:    session.getMouseX(),
-          y:    session.getMouseY(),
-          name: session.getName(),
-          key: session.getName()
-        });
-      }
-  
-      socket.emit('cursor', cursorPositions);
-    }, Math.round(1000/30));
-      socket.on('cursor', data => {
-        const session = sessions[data.sessionKey];
-        session.resetTimer();
-        session.setMouseX(data.x);
-        session.setMouseY(data.y);
+      
+          console.log("A connection has been established");
+         const sessionKeys = Object.keys(sessions);
+         const cursorPositions = [];
+         //Loop and update cursorPositions for each User
+         for(let i =0, n = sessionKeys.length; i < n; i ++){
+            const key = sessionKeys[i];
+            const session = sessions[key];
+            cursorPositions.push({
+                x: session.getMouseX(),
+                y: session.getMouseY(),
+                userName: session.getName(),
+                key: session.getName()
+            });
+         }
+         //Emit - send the cursor positions to all other clients connected
+         socket.emit("cursor", cursorPositions);
+        }, Math.round(1000/30));
+
+      socket.on("cursor", data => {
+          console.log("A line is being drawn");
+          const session = sessions[data.sessionKey];
+          session.resetTimer();
+          session.setMouseX(data.x);
+          session.setMouseY(data.y);
       });
+
       socket.on('line', data => {
         const session = sessions[data.sessionKey];
         const lineCoordinates = data.lineCoordinates;
@@ -141,15 +113,13 @@ class Sessions {
             lineCoordinates
         });
       });
-  });
-  http.listen(8080, () => {
-      //When the server is initialized.
-  });
+    });
 
-  //http.listen(8081, () => {
-  //    console.log("Listening on Port 80");
-  //    //Initialise the server
-  //});
+
+  http.listen(8081, () => {
+      console.log("Listening on Port 80");
+      //Initialise the server
+  });
 
   //Export router so API requests can be called from front-end
 module.exports = router;
